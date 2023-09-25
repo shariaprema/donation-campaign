5deb301b8f9a13f1dc6723e93622508b2ce50cad
shariaprema <shariaprema@gmail.com>
2023-09-25 17:01:23 +0600

    completed navbar section

--- src/Layouts/MainLayout/MainLayout.jsx
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../Components/Header/Navbar/Navbar";
 
 const MainLayout = () => {
     return (
-        <div className="max-w-[1300px] mx-auto">
+        <div className="max-w-[1600px] mx-auto">
 
             <Navbar></Navbar>
             <Outlet></Outlet>
